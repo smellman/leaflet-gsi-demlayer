4f0cecbc65248e08261d2bcdcc619ef758064309
Taro Matsuzawa <taro@georepublic.co.jp>
2016-02-12 22:07:20 +0900

    smelldem version

--- leaflet-gsi-demlayer.js
+++ leaflet-gsi-demlayer.js
@@ -1,26 +1,73 @@
 (function() {
 
-  var parseDEM = function(txt) {
+  var parseDEM = function(buffer) {
+    var view = new DataView(buffer);
+    var pos = 0;
+    var _x = view.getUint32(pos, true);
+    pos += Uint32Array.BYTES_PER_ELEMENT; // 4
+    var _y = view.getUint32(pos, true);
+    pos += Uint32Array.BYTES_PER_ELEMENT; // 8
+    var _z = view.getUint8(pos, true);
+    pos += Uint8Array.BYTES_PER_ELEMENT; // 9
+    var maximumHeight = view.getFloat32(pos, true);
+    pos += Float32Array.BYTES_PER_ELEMENT;
+    var minimumHeight = view.getFloat32(pos, true);
+    pos += Float32Array.BYTES_PER_ELEMENT;
+
+    // availableは飛ばす
+    var _availables_length = 256*256 / 8;
+    var a_pos = pos;
+    pos += Uint8Array.BYTES_PER_ELEMENT * _availables_length;
+    // heights
+    var height_counts = view.getUint32(pos, true);
+    pos += Uint32Array.BYTES_PER_ELEMENT;
+    var zigZagDecode = function(value) {
+        return (value >> 1) ^ (-(value & 1));
+    }
+    var encodedHeights = new Uint16Array(height_counts);
+    var height = 0;
+    for (var i = 0; i < height_counts; i++) {
+        height += zigZagDecode(view.getUint16(pos, true));
+        encodedHeights[i] = height;
+        pos += Uint16Array.BYTES_PER_ELEMENT;
+    }
+
+    var max_min = maximumHeight - minimumHeight;
+    var _height = function(value) {
+        return ((value / 32767.0) * (max_min)) + minimumHeight;
+    }
+    var data_pos = 0;
     var dem = [];
-    txt.split("\n").forEach(function(row) {
-      if (row.indexOf(",") == -1)
-        return;
-      var b = [];
-      row.split(",").forEach(function(col) {
-        b.push(parseFloat(col));
-      });
-      dem.push(b);
-    });
+    // availableの値をパースしながら入れていく
+    var j_max = 256/8;
+    for (var i = 0; i < 256; i++) {
+        var b = [];
+        for (var j = 0; j < j_max; j++) {
+            var a = view.getUint8(a_pos, true);
+            a_pos += Uint8Array.BYTES_PER_ELEMENT;
+            for (k = 0; k < 8; k++) {
+                if ((a & (1 << k)) != 0) {
+                    b.push(_height(encodedHeights[data_pos]));
+                    data_pos += 1;
+                } else {
+                    b.push(NaN);
+                }
+            }
+        }
+        dem.push(b);
+    }
+
     return dem;
   };
 
   var getDEM = function(url) {
     return new Promise(function(resolve, reject) {
       var x = new XMLHttpRequest();
-      x.onreadystatechange = function() {
+      x.responseType = "arraybuffer";
+      x.onload = function() {
         if (x.readyState == 4) {
           if (x.status == 200)
-            resolve(parseDEM(x.responseText));
+            resolve(parseDEM(x.response));
           else
             reject();
         }
@@ -38,7 +85,8 @@
     createTile : function(coords, done) {
 
       var img = L.DomUtil.create('img', 'leaflet-tile');
-      var url = L.Util.template("http://cyberjapandata.gsi.go.jp/xyz/dem/{z}/{x}/{y}.txt", coords);
+      var url = L.Util.template("http://tiles.smellman.org/smelldem/{z}/{x}/{y}.smelldem", coords);
+      //var url = L.Util.template("http://localhost:8007/output4/{z}/{x}/{y}.smelldem", coords);
       var loading = this.loading;
       if (!loading[url])
         loading[url] = getDEM(url);
